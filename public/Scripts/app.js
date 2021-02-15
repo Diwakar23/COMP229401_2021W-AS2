58b0d2b3fd96d0218d3f09f70a27e86cf810ab9c
Diwakar23 <diwakar232496@gmail.com>
2021-02-14 23:22:16 -0500

    Final Commit

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,12 +1,12 @@
 //IIFE - Immediately Invoked Function Expression
 (function(){
+    function Start(){
+        console.log("App Started....");
+    }
+    window.addEventListener("load,Start");
+})();
 
-function Start() 
-{
-
-console.log("App Started....");
-}
-
-window.addEventListener("load,Start");
-
+(function submitForm(that){
+    confirm("Thank you " + that.fname.value + "\n" + that.lname.value + "  for contacting us." + that.cNumber.value + "\n"+ that.email.value + "\n"+ that.message.value);
+    return false ; 
 })();
--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/services', function(req, res, next) {
 });
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Us'});
+  res.render('contact', { title: 'Contact'});
 });
 
 module.exports = router;
